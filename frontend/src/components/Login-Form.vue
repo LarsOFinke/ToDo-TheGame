e3ec5917e984aa7bd6c98c33d15de79669418d2f
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-06-05 13:55:24 +0200

    Implemented showPassword-functionality for the Login-Form

--- frontend/src/components/Login-Form.vue
+++ frontend/src/components/Login-Form.vue
@@ -3,14 +3,22 @@
         <h2 class="text-2xl font-semibold text-gray-800 mb-4 text-center">Login</h2>
         <form @submit.prevent="handleLogin">
             <div class="mb-4">
-                <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
-                <input v-model.trim="username" type="username" id="username" required
-                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+                <label class="block text-sm font-medium text-gray-700">Username
+                    <input v-model.trim="username" type="text" required
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+                </label>
             </div>
             <div class="mb-6">
-                <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                <input v-model.trim="password" type="password" id="password" required
-                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+                <label class="block text-sm font-medium text-gray-700">Password
+                    <input v-model.trim="password" :type="showPassword ? 'text' : 'password'" type="password" required
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+                </label>
+            </div>
+            <div class="mb-16 ">
+                <label class="float-right">
+                    Show password
+                    <input v-model="showPassword" type="checkbox">
+                </label>
             </div>
             <button type="submit"
                 class="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 transition">
@@ -25,6 +33,7 @@ import { ref } from 'vue'
 
 const username = ref('')
 const password = ref('')
+const showPassword = ref(false)
 
 const handleLogin = () => {
     console.log('Logging in with:', username.value, password.value)
