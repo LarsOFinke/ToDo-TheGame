814bff6b33cd1027664f51a29b8b9ba584234227
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-06-05 20:36:40 +0200

    Small navbar CSS changes

--- frontend/src/components/shared/navbar/Navbar-Item.vue
+++ frontend/src/components/shared/navbar/Navbar-Item.vue
@@ -1,14 +1,14 @@
 <template>
-  <nav class="flex bg-gray-800 text-white p-4">
+  <nav class="flex bg-gray-800 text-white p-2">
     <div class="container mx-auto">
       <h1 class="text-xl">PageHeader</h1>
     </div>
 
     <div class="flex float-right">
       <ul class="flex">
-        <li class="mr-2"><a href="/">Home</a></li>
-        <li class="mr-2"><a href="/registration">Registration</a></li>
-        <li class="mr-2"><a href="/tasks">Tasks</a></li>
+        <li class="mr-4 font-medium dark:text-yellow-300 hover:text-yellow-400 hover:underline"><a href="/">Home</a></li>
+        <li class="mr-4 font-medium dark:text-yellow-300 hover:text-yellow-400 hover:underline"><a href="/registration">Registration</a></li>
+        <li class="mr-4 font-medium dark:text-yellow-300 hover:text-yellow-400 hover:underline"><a href="/tasks">Tasks</a></li>
       </ul>
     </div>
   </nav>
