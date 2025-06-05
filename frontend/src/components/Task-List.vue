f2678bce4fa9255f56093a4e345a5d4b5764195c
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-06-05 21:21:22 +0200

    Implemented a scroll-container for the Task-List and improved its CSS generally

--- frontend/src/components/Task-List.vue
+++ frontend/src/components/Task-List.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class="w-full max-w-sm bg-gray-100 rounded-lg shadow-md p-6 mx-auto">
+    <div class="w-full max-w-sm bg-gray-100 rounded-lg shadow-md p-6 mx-auto relative">
         <h2 class="text-2xl font-bold text-gray-800 mb-4 text-center">Task-List</h2>
 
+        <!-- Utility-Buttons -->
+        <div class="flex justify-center mb-4">
+            <button type="button" @click.prevent="showTaskForm"
+                class="text-l w-fit bg-yellow-600 text-white px-2 rounded-md hover:bg-yellow-800 transition">
+                Add new task
+            </button>
+        </div>
 
-
-        <ul v-for="task in taskList" :key="task.id" class="relative m-4">
-            <!-- Utility-Buttons -->
-            <div class="mb-2 absolute top-2 right-[calc(31.5%)]">
-                <button type="button" @click.prevent="showTaskForm"
-                    class="text-l w-fit bg-yellow-600 text-white px-2 rounded-md hover:bg-yellow-800 transition">
-                    Add new task
-                </button>
-            </div>
-
-            <br> <br>
-            <task-item :task="task"></task-item>
-        </ul>
+        <!-- Scrollable Task List -->
+        <div class="overflow-x-auto max-h-95">
+            <scrollbar-hide>
+                <ul v-for="task in taskList" :key="task.id" class="mb-8">
+                    <task-item :task="task"></task-item>
+                </ul>
+            </scrollbar-hide>
+        </div>
     </div>
 </template>
+
 
 <script setup>
 import { ref } from 'vue';
@@ -34,6 +37,21 @@ const taskList = ref([
         deadlineDate: "06.06.2025",
         remainingTime: "2d 4h 24min 12s",   // Needs to get calculated in the Task-Item asynchronously?
         description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime suscipit, et fugiat
+                    ipsam, tempora animi
+                    laborum laboriosam natus asperiores aspernatur itaque cupiditate voluptatum accusamus iure officiis
+                    quisquam obcaecati culpa error?`
+    },
+    {
+        id: 2,
+        title: "Task-Title",
+        mode: "Private / Team",
+        priority: "Priority",
+        deadlineDate: "06.06.2025",
+        remainingTime: "2d 4h 24min 12s",   // Needs to get calculated in the Task-Item asynchronously?
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime suscipit, et fugiat
+                    ipsam, tempora animi
+                    laborum laboriosam natus asperiores aspernatur itaque cupiditate voluptatum accusamus iure officiis
+                    quisquam obcaecati culpa error?Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime suscipit, et fugiat
                     ipsam, tempora animi
                     laborum laboriosam natus asperiores aspernatur itaque cupiditate voluptatum accusamus iure officiis
                     quisquam obcaecati culpa error?`
