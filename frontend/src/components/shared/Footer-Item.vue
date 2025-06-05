75159b70da5f4b645aadc1184a84ba83456d0e4d
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-06-05 20:38:25 +0200

    Footer - adjusted Impressum-link-click-width and Footer-height

--- frontend/src/components/shared/Footer-Item.vue
+++ frontend/src/components/shared/Footer-Item.vue
@@ -1,8 +1,8 @@
 <template>
-    <footer class="flex bg-gray-800 text-white p-4 w-full">
+    <footer class="flex bg-gray-800 text-white p-2 w-full">
         <div class="w-full flex text-xs text-white ">
             <p class="w-full">©2025 Lars-O. Finke & Bojan Bader</p>
-            <a class="w-full text-right font-medium dark:text-yellow-300 hover:text-yellow-400 hover:underline float-right self-center" href="/impressum">
+            <a class="text-right font-medium dark:text-yellow-300 hover:text-yellow-400 hover:underline float-right self-center" href="/impressum">
                 Impressum
             </a>
         </div>
