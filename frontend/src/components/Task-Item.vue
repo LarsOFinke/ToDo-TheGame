f960858c5ef47a8a712ef414e53deba0fa79ca34
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-06-08 22:28:11 +0200

    Implemented blueprint for To-Do-List in the Task-Item and a toggle to switch between the task-description and the task-to-dos

--- frontend/src/components/Task-Item.vue
+++ frontend/src/components/Task-Item.vue
@@ -2,7 +2,7 @@
     <div class="w-full max-w-sm bg-gray-200 rounded-lg shadow-md p-6 mx-auto relative">
         <br>
 
-        <!-- Task header -->
+        <!-- Task-Header -->
         <div class="flex items-center justify-between w-full mb-2">
             <h3 class="text-xl font-bold text-gray-800 mb-2">{{ task.title }}</h3>
             <div class="text-sm font-semibold text-gray-800 mb-2 rounded-lg shadow-md p-1">
@@ -11,7 +11,7 @@
             </div>
         </div>
 
-        <!-- Task info -->
+        <!-- Task-Info -->
         <div class="flex text-xs mb-4">
             <div class="w-full">
                 <label class="font-semibold">Deadline:</label>
@@ -32,14 +32,33 @@
             </div>
         </div>
 
-        <!-- Task description -->
-        <div>
-            <h4 class="font-semibold">Description:</h4>
-            <div class="text-sm mb-4 overflow-x-auto h-26">
-                <p class="overflow-y-hidden scrollbar-hide">{{ task.description }}</p>
+        <div class="flex flex-col">
+            <!-- Control-Buttons -->
+            <div class="w-full flex justify-around">
+                <button type="button" @click.prevent="toggleShowDescription(true)"
+                    class="w-50 bg-indigo-600 text-white py-1 px-4 rounded-md hover:bg-indigo-800 transition">Description</button>
+                <button type="button" @click.prevent="toggleShowDescription(false)"
+                    class="w-50 bg-green-600 text-white py-1 px-2 rounded-md hover:bg-green-800 transition">To-Do's</button>
+            </div>
+
+            <!-- Task-Description -->
+            <div v-if="showDescription">
+                <h4 class="font-semibold">Description:</h4>
+                <div class="text-sm mb-4 overflow-x-auto h-18">
+                    <p class="overflow-y-hidden scrollbar-hide">{{ task.description }}</p>
+                </div>
+            </div>
+
+            <!-- To-Do-List -->
+            <div v-else>
+                <h4 class="font-semibold">To-Do's:</h4>
+                <div class="text-sm mb-4 overflow-x-auto h-18">
+                    <ul v-for="todo in todoList" :key="todo.id">
+                        <li :value="todo.id">{{ todo.text }}</li>
+                    </ul>
+                </div>
             </div>
         </div>
-
 
         <!-- Utility-Buttons -->
         <div class="mb-2 absolute top-2 right-2">
@@ -57,14 +76,23 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useTasksService } from '@/services/TasksService'
 
 const emit = defineEmits(['hideItemEdit', 'updateTaskList', 'closeItem'])
 const { deleteTask, closeTask } = useTasksService()
-
 const { task } = defineProps({
     task: Object
 })
+const showDescription = ref(true)
+
+const toggleShowDescription = (toggleOn) => {
+    if (toggleOn) {
+        showDescription.value = true
+    } else {
+        showDescription.value = false
+    }
+}
 
 const showItemEdit = () => {
     emit('hideItemEdit', [false, task])
