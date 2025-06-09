1a4044b57607fb00ccda68e133d6d4121733cfbd
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-06-09 14:18:47 +0200

    Small bug fix causing Flask not to start.

--- backend/crud.py
+++ backend/crud.py
@@ -4,6 +4,53 @@ from . import logging  # GET .ENV CONSTANTS AND LOGGING
 from werkzeug.security import generate_password_hash, check_password_hash # User-SYSTEM SECURITY
 
 
+#-- EXECUTE SQL --#
+def execute_query(sql: str, params: Tuple[Any, ...], connectionstring: str, fetch: bool = False) -> Optional[List[Tuple]]:
+    """Executes a SQL command with the provided parameters.
+
+    This function connects to the SQLite database, executes the specified SQL command with the given parameters,
+    and optionally fetches the results based on the `fetch` argument. 
+
+    Args:
+        sql (str): The SQL-Command to be executed. This can be any valid SQL statement such as 
+                   SELECT, INSERT, UPDATE, or DELETE.
+                   
+        connectionstring (str): The DB-Connection-String used for the query
+                   
+        params (Tuple[Any, ...]): A tuple containing the parameters to bind to the SQL command. 
+                                The number and types of parameters should match the placeholders in the SQL statement.
+                                
+        fetch (bool, optional): If True, the function fetches and returns all rows of the result set as a list of tuples.
+                                If False, it returns a boolean indicating whether any rows were affected by 
+                                the SQL command (True for affected, False for none). Defaults to False.
+    Returns:
+        Optional[List[Tuple]]: 
+            - If `fetch` is True, returns a list of tuples where each tuple represents a row fetched from the database. 
+              Returns an empty list if no rows were returned.
+            - If `fetch` is False, returns a boolean value:
+              - True if the SQL command affected one or more rows.
+              - False if no rows were affected.
+            - Returns None if an error occurs during execution.
+
+    """
+    try:
+        with sqlite3.connect(connectionstring) as con:
+            cursor = con.cursor()
+            cursor.execute("PRAGMA foreign_keys = ON;")
+            con.commit()
+            cursor.execute(sql, params)
+            
+            if fetch:
+                return cursor.fetchall()  # Return fetched results
+            
+            else:
+                return cursor.rowcount > 0  # Return True for insert/update/delete if any rows affected, else False
+            
+    except sqlite3.Error as e:
+        logging.error(f"An error occurred: {e}")
+        return None  # Return None in case of error
+
+
 ### SET UP DATABASES ###
 DB_DIRECTORY = os.path.join(os.path.dirname(__file__), "db")
 
@@ -107,53 +154,6 @@ if not os.path.exists(CONNECTIONSTRING):
           
  
 ### SET UP CRUD-FUNCTIONALITY ###
-
-#-- EXECUTE SQL --#
-def execute_query(sql: str, params: Tuple[Any, ...], connectionstring: str, fetch: bool = False) -> Optional[List[Tuple]]:
-    """Executes a SQL command with the provided parameters.
-
-    This function connects to the SQLite database, executes the specified SQL command with the given parameters,
-    and optionally fetches the results based on the `fetch` argument. 
-
-    Args:
-        sql (str): The SQL-Command to be executed. This can be any valid SQL statement such as 
-                   SELECT, INSERT, UPDATE, or DELETE.
-                   
-        connectionstring (str): The DB-Connection-String used for the query
-                   
-        params (Tuple[Any, ...]): A tuple containing the parameters to bind to the SQL command. 
-                                The number and types of parameters should match the placeholders in the SQL statement.
-                                
-        fetch (bool, optional): If True, the function fetches and returns all rows of the result set as a list of tuples.
-                                If False, it returns a boolean indicating whether any rows were affected by 
-                                the SQL command (True for affected, False for none). Defaults to False.
-    Returns:
-        Optional[List[Tuple]]: 
-            - If `fetch` is True, returns a list of tuples where each tuple represents a row fetched from the database. 
-              Returns an empty list if no rows were returned.
-            - If `fetch` is False, returns a boolean value:
-              - True if the SQL command affected one or more rows.
-              - False if no rows were affected.
-            - Returns None if an error occurs during execution.
-
-    """
-    try:
-        with sqlite3.connect(connectionstring) as con:
-            cursor = con.cursor()
-            cursor.execute("PRAGMA foreign_keys = ON;")
-            con.commit()
-            cursor.execute(sql, params)
-            
-            if fetch:
-                return cursor.fetchall()  # Return fetched results
-            
-            else:
-                return cursor.rowcount > 0  # Return True for insert/update/delete if any rows affected, else False
-            
-    except sqlite3.Error as e:
-        logging.error(f"An error occurred: {e}")
-        return None  # Return None in case of error
-
 
 #-- User --#
 def create_user(username: str, password: str) -> bool:
