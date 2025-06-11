5dd0154888a032003bedca5317c08a343075e9ab
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-06-11 18:12:24 +0200

    Implemented a scroll-container to display the teams-info and small CSS changes to Teams-Item itself.

--- frontend/src/components/teams/Teams-Item.vue
+++ frontend/src/components/teams/Teams-Item.vue
@@ -6,20 +6,29 @@
         </div>
 
         <!-- Teams-Info -->
-        <div class="flex text-xs mb-4">
-            <div class="flex flex-col w-45">
-                <label class="mb-2"><u>Founder:</u> Name</label>
-                <label class="mb-2"><u>Info#2:</u> Infopjmdnipsanpid</label>
-                <label class="mb-2"><u>Info#3:</u> KLloihsdulif sdagf ghrdg</label>
-                <label class="mb-2"><u>Info#4:</u> Ohouibhg oughns ubnsgu bdgub klbgu kubgk g jkkjbb hb</label>
-            </div>
-            <div class="flex float-right">
-                <div class="w-full">
-                    <label class="text-sm font-semibold">Members:</label>
-                    <p class="text-sm text-right">27</p>
+        <div class="w-full max-w-sm bg-gray-100 rounded-lg shadow-md p-2 mx-auto relative mb-2">
+            <div class="text-sm mb-4 overflow-x-auto  max-h-57">
+                <div class="flex text-xs mb-4">
+                    <div class="flex flex-col w-35">
+                        <label class="mb-2"><u>Founder:</u> Name</label>
+                        <label class="mb-2"><u>Info#2:</u> Infopjmdnipsanpid</label>
+                        <label class="mb-2"><u>Info#3:</u> KLloihsdulif sdagf ghrdg</label>
+                        <label class="mb-2"><u>Info#2:</u> Infopjmdnipsanpid</label>
+                        <label class="mb-2"><u>Info#3:</u> KLloihsdulif sdagf ghrdg</label>
+                        <label class="mb-2"><u>Info#2:</u> Infopjmdnipsanpid</label>
+                        <label class="mb-2"><u>Info#3:</u> KLloihsdulif sdagf ghrdg</label>
+                        <label class="mb-2"><u>Info#4:</u> Ohouibhg oughns ubnsgu bdgub klbgu kubgk g jkkjbb hb</label>
+                    </div>
+                    <div class="flex float-right mr-1">
+                        <div class="w-full">
+                            <label class="text-sm font-semibold">Members:</label>
+                            <p class="text-sm text-right">27</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
 
         <!-- Utility-Buttons -->
         <div class="mb-2 absolute top-2 right-2">
@@ -28,7 +37,7 @@
         </div>
         <div class="w-full flex justify-around">
             <button type="button" @click.prevent="showTeamTasks"
-                class="w-fit bg-green-600 text-white py-1 px-2 rounded-md hover:bg-green-800 transition">Team-Tasks</button>
+                class="w-fit bg-indigo-600 text-white py-1 px-2 rounded-md hover:bg-indigo-800 transition">Team-Tasks</button>
         </div>
     </div>
 </template>
